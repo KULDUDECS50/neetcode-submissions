9883433d2f6dab5933cd7b2b842bd6904812d225
Kuldeep Debnath <63112459+KULDUDECS50@users.noreply.github.com>
2026-08-26 12:41:36 -0500

    Add: minimum-stack - submission-14

---
+++ Data Structures & Algorithms/minimum-stack/submission-14.java
@@ -0,0 +1,31 @@
+class MinStack {
+
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(minStack.isEmpty() || minStack.peek() >= val){
+            minStack.push(val);
+        }
+        stack.push(val);
+    }
+    
+    public void pop() {
+        if(!minStack.isEmpty() && (minStack.peek().equals(stack.pop()))){
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
