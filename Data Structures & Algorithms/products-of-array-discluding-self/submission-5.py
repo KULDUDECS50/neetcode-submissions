835d19ca94f6584694a5e6e11fc61b4cc1904ff5
Kuldeep Debnath <63112459+KULDUDECS50@users.noreply.github.com>
2026-09-14 15:54:15 -0500

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        arr1 = [1] * len(nums)
+        arr2 = [1] * len(nums)
+        # first pass to multiple back
+
+        runningPass = 1
+        for i in range(1, len(nums)):
+            runningPass *= nums[i-1]
+            arr1[i] = runningPass
+        
+        # second pass to multiple back
+
+        runningPass = 1
+        for i in range(len(nums)-2, -1,-1):
+            runningPass *= nums[i+1]
+            arr2[i] = runningPass
+
+        res = [0] * len(nums)
+
+        for i in range(0,len(nums)):
+            res[i] = arr1[i] * arr2[i]
+        return res
+
+        
